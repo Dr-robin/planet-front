5d1163fff6fbb8b8c825c134f2755cfe8f96ee49
robin <robin@makichan.moe>
2017-06-25 23:54:56 +0900

    로그인 여부에 따라 메뉴 분기 추가

--- src/components/nav-top.vue
+++ src/components/nav-top.vue
@@ -2,28 +2,34 @@
 	<section>
 		<nav class="navbar navbar-default navbar-fixed-top">
 			<ul class="nav navbar-nav navbar-left">
-				<li class="visible-xs-block"><a v-on:click="isMenuOpen = !isMenuOpen"><i class="fa fa-bars"></i></a></li>
+				<li class="visible-xs-block"><a @click="isMenuOpen = !isMenuOpen"><i class="fa fa-bars"></i></a></li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
 				<li><a href="#"><i class="fa fa-bell-o"></i></a></li>
 			</ul>
 		</nav>
-		<div id="bar-menu" v-bind:class="{active: isMenuOpen}">
+		<div id="bar-menu" :class="{active: isMenuOpen}">
 			<div class="container">
-				<ul>
+				<ul v-if="user.isLogin">
 					<li><a href="#"><i class="fa fa-television"></i><span>메뉴1</span></a></li>
 					<li><a href="#"><i class="fa fa-globe"></i><span>메뉴2</span></a></li>
 					<li><a href="#"><i class="fa fa-users"></i><span>메뉴3</span></a></li>
+				</ul>
+				<ul v-else>
+					<li><router-link to="/login"><i class="fa fa-sign-in"></i><span>로그인</span></router-link></li>
 				</ul>
 			</div>
 		</div>
 	</section>
 </template>
 <script>
+	import userData from '../models/userData';
+
 	export default {
 		data() {
 			return {
-				isMenuOpen: false
+				isMenuOpen: false,
+				user: userData.data
 			}
 		}
 	}
