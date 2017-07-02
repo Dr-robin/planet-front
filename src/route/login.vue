effdbc2d8958137d15b991fe7c2405c90e9f1b68
robin <robin@makichan.moe>
2017-07-02 19:17:27 +0900

    안타-노시!한 로그인 페이지 제작

---
+++ src/route/login.vue
@@ -0,0 +1,40 @@
+<template>
+	<div id="panel-login" class="panel panel-default">
+		<div class="panel-heading">로그인</div>
+		<div class="panel-body">
+			<form @submit.prevent="login">
+				<div class="form-group">
+					<label for="input-email">이메일</label>
+					<input type="email" class="form-control" id="input-email" v-model="form.email" placeholder="maki@planet.moe">
+				</div>
+				<div class="form-group">
+					<label for="input-password">비밀번호</label>
+					<input type="password" class="form-control" id="input-password" v-model="form.password" placeholder="********">
+				</div>
+				<button type="submit" class="btn btn-default">로그인</button>
+			</form>
+		</div>
+	</div>
+</template>
+<script>
+import $ from 'jquery';
+import user from '../models/userData';
+import router from '../main';
+
+export default {
+	data() {
+		return {
+			form: {}
+		}
+	},
+	methods: {
+		login() {
+			if(this.form.email && this.form.password) {
+				user.login(this.form.email, this.form.password).then(() => {
+					router.push('/home');
+				});
+			}
+		}
+	}
+}
+</script>
