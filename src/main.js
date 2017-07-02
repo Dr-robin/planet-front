16892f2525038de71a317d0dcef587369347e0ed
robin <robin@makichan.moe>
2017-07-02 19:16:13 +0900

    로그인 URL 추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import main from './route/main.vue'
+import main from './route/main.vue';
+import login from './route/login.vue';
 
 Vue.use(VueRouter);
 require('./load-component');
@@ -9,7 +10,8 @@ require('./load-component');
 const router = new VueRouter({
 	routes: [
 		{path: '/', component: main},
-		{path: '/home', component: main}
+		{path: '/home', component: main},
+		{path: '/login', component: login}
 	],
 	mode: 'history'
 });
