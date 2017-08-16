5e35f06d4fd9a1b38aca0d3d5cf009a77cf2fe3c
robin <robin@makichan.moe>
2017-08-17 00:09:53 +0900

    닉네임 변수 경로 오타 수정

--- src/components/nav-top.vue
+++ src/components/nav-top.vue
@@ -13,7 +13,7 @@
 				<ul v-if="user.isLogin">
 					<li><a href="#"><i class="fa fa-television"></i><span>게임</span></a></li>
 					<li><a href="#"><i class="fa fa-globe"></i><span>커뮤니티</span></a></li>
-					<li><a href="#"><i class="fa fa-users"></i><span>{{user.nickname}}</span></a></li>
+					<li><a href="#"><i class="fa fa-users"></i><span>{{user.data.nickname}}</span></a></li>
 				</ul>
 				<ul v-else>
 					<li><a href="#" @click="login()"><i class="fa fa-sign-in"></i><span>로그인</span></a></li>
