d168f899ea42b4838ac900224aa5dd471ea212bf
robin <robin@makichan.moe>
2017-09-21 21:38:49 +0900

    이것도 매터리얼화

--- src/components/nav-top.vue
+++ src/components/nav-top.vue
@@ -1,26 +1,15 @@
 <template>
-	<section>
-		<nav class="navbar navbar-default navbar-fixed-top">
-			<ul class="nav navbar-nav navbar-left">
-				<li class="visible-xs-block"><a @click="isMenuOpen = !isMenuOpen"><i class="fa fa-bars"></i></a></li>
-			</ul>
-			<ul class="nav navbar-nav navbar-right" v-if="user.isLogin">
-				<li><a href="#"><i class="fa fa-bell-o"></i></a></li>
-			</ul>
-		</nav>
-		<div id="bar-menu" :class="{active: isMenuOpen}">
-			<div class="container">
-				<ul v-if="user.isLogin">
-					<li><a href="#"><i class="fa fa-television"></i><span>게임</span></a></li>
-					<li><a href="#"><i class="fa fa-globe"></i><span>커뮤니티</span></a></li>
-					<li><a href="#"><i class="fa fa-users"></i><span>{{user.data.nickname}}</span></a></li>
-				</ul>
-				<ul v-else>
-					<li><a href="/login"><i class="fa fa-sign-in"></i><span>로그인</span></a></li>
-				</ul>
-			</div>
-		</div>
-	</section>
+	<md-toolbar>
+		<md-button class="md-icon-button">
+			<md-icon>menu</md-icon>
+		</md-button>
+
+		<h2 class="md-title" style="flex: 1">플래닛</h2>
+
+		<md-button class="md-icon-button">
+			<md-icon>favorite</md-icon>
+		</md-button>
+	</md-toolbar>
 </template>
 <script>
 	import userData from '../models/userData';
@@ -29,7 +18,6 @@
 	export default {
 		data() {
 			return {
-				isMenuOpen: false,
 				user: userData.data
 			}
 		},
