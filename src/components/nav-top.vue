75ad9cdb25dbe9eeb76b0933012498878dae4a94
robin <robin@makichan.moe>
2017-09-26 23:24:36 +0900

    프로필 메뉴 제작

--- src/components/nav-top.vue
+++ src/components/nav-top.vue
@@ -1,11 +1,23 @@
 <template>
-	<md-toolbar>
-		<md-button class="md-icon-button">
-			<md-icon>menu</md-icon>
-		</md-button>
-		<h2 class="md-title" style="flex: 1">플래닛</h2>
-		<router-link tag="md-button" to="/login" v-if="!user.isLogin">로그인</router-link>
-	</md-toolbar>
+	<section>
+		<md-toolbar>
+			<h2 class="md-title" style="flex: 1">플래닛</h2>
+			<router-link tag="md-button" to="/login" v-if="!user.isLogin">로그인</router-link>
+			<md-button class="md-icon-button" v-else @click="toggleSideMenu">
+				<md-icon>menu</md-icon>
+			</md-button>
+		</md-toolbar>
+		<md-sidenav class="md-right" ref="sidenav">
+			<md-list>
+				<md-list-item>
+					<md-avatar>
+						<img src="https://figuya.com/uploads/product/profile_picture/7692/profile_Nendoroid_692_You_Watanabe_vorschau.jpg" alt="profile">
+					</md-avatar>
+					<span>{{user.data.name}}</span>
+				</md-list-item>
+			</md-list>
+		</md-sidenav>
+	</section>
 </template>
 <script>
 	import userData from '../models/userData';
@@ -23,6 +35,9 @@
 					localStorage.setItem('session', data.sessID);
 					window.location.replace('https://api.planet.moe/auth/' + data.sessID);
 				});
+			},
+			toggleSideMenu() {
+				this.$refs.sidenav.toggle();
 			}
 		}
 	}
